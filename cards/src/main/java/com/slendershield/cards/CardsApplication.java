c79ef639eacf6f7d646f1b3fbc96658359684a0a
Muralidhara Bhat KS <ksmuralidhara0@gmail.com>
2025-03-24 18:22:13 +0530

    Cards microservice implementation.

--- cards/src/main/java/com/slendershield/cards/CardsApplication.java
+++ cards/src/main/java/com/slendershield/cards/CardsApplication.java
@@ -2,8 +2,37 @@ package com.slendershield.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Cards microservice REST API Documentation",
+				description = "MDB's Cards microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Muralidhara Bhat KS",
+						email = "ksmuralidhara0@gmail.com",
+						url = "https://www.muralidharabhat.in"
+				),
+				license = @License(
+						name = "MIT License",
+						url = "https://opensource.org/licenses/MIT"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Github Repo",
+				url = "https://github.com/slendershield"
+		)
+)
 public class CardsApplication {
 
 	public static void main(String[] args) {
